1dba2308365c9c18ecb1b9543084ce226116ab86
Carlos Alberto Nin Queliz <128722564+Carlosnin0623@users.noreply.github.com>
2025-09-14 10:21:24 -0400

    Videos con bootrsap

---
+++ 08-Mantener-el-ratio-de-un-video/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using _08_Mantener_el_ratio_de_un_video.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _08_Mantener_el_ratio_de_un_video.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 08-Mantener-el-ratio-de-un-video/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _08_Mantener_el_ratio_de_un_video.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
