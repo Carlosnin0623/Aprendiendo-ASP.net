462a11d6117fa14b28bb28a8caea58f72e2582e9
Carlos Alberto Nin Queliz <128722564+Carlosnin0623@users.noreply.github.com>
2025-09-14 14:01:44 -0400

    Tamaños con boostrap

---
+++ 09-Tamaños-en-bootrap/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _09_Tamaños_en_bootrap.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
