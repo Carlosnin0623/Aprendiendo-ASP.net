9ed48d43beb1fa2d9e8727891234beda2176f19d
Carlos Alberto Nin Queliz <128722564+Carlosnin0623@users.noreply.github.com>
2025-09-14 09:47:42 -0400

    Tablas en Boostrap

---
+++ 07-Tablas-Boostrap/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _07_Tablas_Boostrap.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
