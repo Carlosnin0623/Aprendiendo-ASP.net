de19b5498f3880151f599c921ed691a72c298c43
Carlos Alberto Nin Queliz <128722564+Carlosnin0623@users.noreply.github.com>
2025-09-20 17:15:09 -0400

    Tabs en Bootstrap

---
+++ 17-Tabs/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _17_Tabs.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
