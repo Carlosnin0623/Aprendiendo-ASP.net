c9ea6ff97b3f33af37463d2fbd96b73e00e41204
Carlos Alberto Nin Queliz <128722564+Carlosnin0623@users.noreply.github.com>
2025-09-20 19:46:38 -0400

    Toats con Bootrap

---
+++ 19-Mostrando-Toats/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _19_Mostrando_Toats.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
