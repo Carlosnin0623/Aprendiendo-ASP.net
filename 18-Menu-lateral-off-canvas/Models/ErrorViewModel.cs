fa0fd5a7c71c6208afabbbc9b76b4e4a8dfd2d1e
Carlos Alberto Nin Queliz <128722564+Carlosnin0623@users.noreply.github.com>
2025-09-20 19:27:06 -0400

    Menu latetal Off Canvas

---
+++ 18-Menu-lateral-off-canvas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _18_Menu_lateral_off_canvas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
