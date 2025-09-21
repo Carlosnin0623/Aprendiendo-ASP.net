34beea22ab85cd8a4da62157fe2b0b084afc8a59
Carlos Alberto Nin Queliz <128722564+Carlosnin0623@users.noreply.github.com>
2025-09-21 16:13:03 -0400

    Portafolio

---
+++ 20-Proyecto-Portafolio/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+using System.Diagnostics;
+using _20_Proyecto_Portafolio.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _20_Proyecto_Portafolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            
+            /* Pasando datos a la vista usando un modelo fuertemente tipado */
+
+            var persona = new Persona()
+            {
+                Nombre = "Carlos Alberto",
+                Edad = 28
+            };
+
+            return View(persona);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 20-Proyecto-Portafolio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _20_Proyecto_Portafolio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
